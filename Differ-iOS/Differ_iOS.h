c74dad799f8dd3ed150accca3a21dbd00ed49a06
ryosukemiyazawa <ryosukemiyazawa@gmail.com>
2021-03-23 11:53:49 +0900

    create differ-ios and delete default target(replace)

---
+++ Differ-iOS/Differ_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Differ_iOS.h
+//  Differ-iOS
+//
+//  Created by Miyazawa on 2021/03/23.
+//  Copyright © 2021 Differ Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Differ_iOS.
+FOUNDATION_EXPORT double Differ_iOSVersionNumber;
+
+//! Project version string for Differ_iOS.
+FOUNDATION_EXPORT const unsigned char Differ_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Differ_iOS/PublicHeader.h>
+
+
